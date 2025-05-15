63f01b88671a27df2aabb80cb00810b2bbcb2c4c
Roberto Perata <roberto.perata@milkmantechnologies.com>
2025-05-15 11:15:17 +0200

    add swagger to project

---
+++ README.md
@@ -0,0 +1,5 @@
+# EMS
+
+## Swagger
+url: http://localhost:8080/swagger-ui/index.html
+
--- src/main/java/net/perata/fleet_manager/controller/VehicleController.java
+++ src/main/java/net/perata/fleet_manager/controller/VehicleController.java
@@ -1,7 +1,27 @@
 package net.perata.fleet_manager.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import net.perata.fleet_manager.domain.Vehicle;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.function.EntityResponse;
 
 @RestController
+@RequestMapping("/vehicles")
 public class VehicleController {
+
+    @Operation(summary = "Get all vehicles")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of vehicles",
+            content = @Content(schema = @Schema(implementation = Vehicle.class)))
+    })
+    @GetMapping( produces = "application/json")
+    public EntityResponse<Vehicle> getVehicles() {
+        return null;
+    }
 }
